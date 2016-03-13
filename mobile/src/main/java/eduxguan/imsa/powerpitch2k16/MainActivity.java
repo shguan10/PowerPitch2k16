7a1bee2932e12c03edafa1aeaceadaf0380787b7
Xinyu <shguan10@gmail.com>
2016-03-13 13:50:09 -0500

    Made a click counter in the mobile version

--- mobile/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
+++ mobile/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int clicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +22,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });*/
+        final TextView text = (TextView) findViewById(R.id.buttonPressedText);
+        final ImageButton button = (ImageButton) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked++;
+                text.setText("The Button has been pressed " + clicked + " times");
+            }
+
         });
+
     }
 
     @Override
--- wear/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
+++ wear/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
@@ -3,11 +3,14 @@ package eduxguan.imsa.powerpitch2k16;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private int clicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,18 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        final TextView text = (TextView) findViewById(R.id.startPositionText);
+        final ImageButton button = (ImageButton) findViewById(R.id.startButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked++;
+                text.setText("The Button has been pressed " + clicked + " times");
+            }
+
+        });
+
     }
 }
