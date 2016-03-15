9739dd4c9893559cdc5d2ab20f895233611d5649
Xinyu <shguan10@gmail.com>
2016-03-14 19:48:11 -0500

    Started using the Accelerometer API

--- mobile/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
+++ mobile/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
@@ -1,5 +1,10 @@
 package eduxguan.imsa.powerpitch2k16;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,17 +16,26 @@ import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    int clicked = 0;
+    //int clicked = 0;
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+    private long diffTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //Below is sample FAB implementation
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +44,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-        final TextView text = (TextView) findViewById(R.id.buttonPressedText);
+
+        //Below is the click counter implementation
+        /*final TextView text = (TextView) findViewById(R.id.buttonPressedText);
         final ImageButton button = (ImageButton) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -40,8 +56,11 @@ public class MainActivity extends AppCompatActivity {
                 text.setText("The Button has been pressed " + clicked + " times");
             }
 
-        });
+        });*/
 
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -65,4 +84,42 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long currentTime = System.currentTimeMillis();
+
+            //Below, to make sure we don't use too many data
+            if ((currentTime - lastUpdate) > 50) {
+                diffTime = (currentTime - lastUpdate);
+                lastUpdate = currentTime;
+            }
+            
+            
+            //Below, not sure what it does
+            float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 }
