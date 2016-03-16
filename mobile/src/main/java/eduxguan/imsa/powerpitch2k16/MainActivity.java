1b923c36f257e002a161c17ff52eaded01977265
Xinyu <shguan10@gmail.com>
2016-03-16 16:08:32 -0500

    Found out that we have threading issues, and we tried to fix that by making a distanceThing thread object
    We also need to incorporate the gyroscope data.

--- mobile/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
+++ mobile/src/main/java/eduxguan/imsa/powerpitch2k16/MainActivity.java
@@ -22,12 +22,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
     private long lastUpdate = 0;
-    private double previousXAcc, previousYAcc, previousZAcc, previousXVel, previousYVel, previousZVel, previousXPos, previousYPos, previousZPos;
+    static private double previousXAcc, previousYAcc, previousZAcc, previousXVel, previousYVel, previousZVel, previousXPos, previousYPos, previousZPos;
     private static final int SHAKE_THRESHOLD = 600;//don't know if we need this
-    private int frequency = 200;
-    private double timeInterval = 1000/frequency;
+    static private int frequency = 200;//frequency every second
+    static private double timeInterval = 1/(frequency);
     private boolean running = false;
     private double lineardistance;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,17 +49,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });*/
 
         final ImageButton button = (ImageButton) findViewById(R.id.button);
-        final TextView text = (TextView) findViewById(R.id.distanceText);
+        text = (TextView) findViewById(R.id.distanceText);
 
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
+        final Thread thread = new Thread(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 running = !running;
+                if(distanceThing.isAlive()) {
+                    try {
+                        distanceThing.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
 
                 if(running){
                     previousXAcc = 0;
@@ -70,10 +78,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     previousXPos = 0;
                     previousYPos = 0;
                     previousZPos = 0; //above, resets the values when accelerometer is "started up" again
+                    try{
+                    distanceThing.start();} catch(Exception e){}//we don't know why we need the try catch, but it works
+
                 } else {
+
                     calculateTotalDistance();
-                    text.setText(lineardistance + " meters");//use whatever to display total distance onscreen
+
                 }
+                text.setText(previousXVel + "");//doesn't print out velocity correctly
             }
 
         });
@@ -121,27 +134,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    public void update() {//updates the "previous acc, vel, pos, values to the current one
+    static public void update() {//updates the "previous" acc, vel, pos, values to the current one. But is this method actually called?
         updateVel();
         updatePos();
     }
 
-    private void updatePos() {
+    static private void updatePos() {
         previousXPos += timeInterval*previousXVel;
         previousYPos += timeInterval*previousYVel;
         previousZPos += timeInterval*previousZVel;
     }
 
-    private void updateVel() {
+    static private void updateVel() {
         previousXVel += timeInterval*previousXAcc;
         previousYVel += timeInterval*previousYAcc;
         previousZVel += timeInterval*previousZAcc;
     }
 
-    private void updateAcc(SensorEvent sensorEvent) {
+    private void updateAcc(SensorEvent sensorEvent) {//we need to use gyroscope data in conjunction with this, because accelerating upwards, is the same as rotating the phone 90 degrees and accelerating rightwards.
         previousXAcc = sensorEvent.values[0];
         previousYAcc = sensorEvent.values[1];
         previousZAcc = sensorEvent.values[2];
+
+        //double linearacceleration = Math.sqrt(previousXAcc* previousXAcc + previousYAcc*previousYAcc + previousZAcc*previousZAcc);
+        //text.setText(previousXAcc+"x ac\n"+previousYAcc+" y acc\n"+previousZAcc+"z acc \n"+linearacceleration + " meters per second squared.");
     }
 
     private void calculateTotalDistance(){
@@ -164,15 +180,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     @Override
-    public void run() {
+    public void run() {//This doesn't work.
         long then = System.nanoTime();
         long timer = System.currentTimeMillis();
-        final double nanos = 1000000000.0 / frequency;
+        final double nanos = 100000000.0 / frequency;
         double delta = 0;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(lineardistance + " meters");//use whatever to display total distance onscreen
+            }
+        });
         while (running) {
             long now = System.nanoTime();
             delta += (now - then) / nanos;
             then = now;
+
             while (delta >= 1) {
                 update();
                 delta--;
@@ -183,4 +206,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         }
     }
+
+    private final Thread distanceThing = new Thread( new Runnable() {
+        @Override
+        public void run() {
+            long then = System.nanoTime();
+            long timer = System.currentTimeMillis();
+            final double nanos = 100000000.0 / frequency;
+            double delta = 0;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    text.setText(lineardistance + " meters");//use whatever to display total distance onscreen
+                }
+            });
+            while (running) {
+                long now = System.nanoTime();
+                delta += (now - then) / nanos;
+                then = now;
+
+                while (delta >= 1) {
+                    update();
+                    delta--;
+                }
+
+                if (System.currentTimeMillis() - timer > 1000) {
+                    timer += 1000;
+                }
+            }
+        }
+    });
 }
